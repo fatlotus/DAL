b179c26145d0cb40a4592e16f82a0bbe515c653c
Jeremy Archer <open-source@fatlotus.com>
2014-04-03 14:46:20 -0500

    Add genomes dataset to repo.

--- DAL/datasets/genomes.py
+++ DAL/datasets/genomes.py
@@ -4,7 +4,7 @@ from s3iterable import S3Iterable
 
 class Genomes(S3Iterable):
   def __init__(self):
-    super(Sou, self).__init__() 
+    super(Genomes, self).__init__() 
     self.config = config.config()
     if config.local():
       self.bucketname = self.config['genomes']['bucket']+'-local'
