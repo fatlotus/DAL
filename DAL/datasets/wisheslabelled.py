e4cf5d8abcef0f03840f8b33a032c9f4cbd2f89f
Jeremy Archer <open-source@fatlotus.com>
2014-05-06 17:28:33 -0500

    Add evaluation function.

--- DAL/datasets/wisheslabelled.py
+++ DAL/datasets/wisheslabelled.py
@@ -12,3 +12,34 @@ class WishesLabelled(S3Iterable):
     else:
       self.bucketname = self.config['wishes-labelled']['bucket']
     self.parser = json.loads
+  
+  def subsets(self):
+    """
+    Returns the subsets of the labelled wishes dataset.
+    """
+    
+    result = []
+    
+    for item in super(WishesLabelled, self).subsets():
+      if item != "oracle.json":
+        result.append(item)
+    
+    return result
+  
+  def eval(self, values):
+    """
+    Returns the fraction of Tweets that were classified correctly, given
+    a dictionary mapping from +tweet_id+'s to classes.
+    """
+    
+    oracle = json.load(self.cache.directhandle("oracle.json", "r"))
+    
+    correct = 0
+    total = 0
+    
+    for key, value in oracle.items():
+      if oracle[key] == values.get(key, None):
+        correct += 1
+      total += 1
+    
+    return float(correct) / total
