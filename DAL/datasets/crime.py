1eb7630c02233dbe33a5b01fa178766996d597b4
Jeremy Archer <open-source@fatlotus.com>
2014-05-27 22:29:55 -0500

    Add crime data.

--- DAL/datasets/crime.py
+++ DAL/datasets/crime.py
@@ -3,18 +3,59 @@ import json
 import pickle
 from cache import Cache
 from s3iterable import S3Iterable
+import csv
+import datetime
 
 class Crime(S3Iterable):
   def __init__(self):
     super(Crime, self).__init__() 
     self.config = config.config()
-    if config.local():
-      self.bucketname = self.config['crime']['bucket']+'-local'
-    else:
-      self.bucketname = self.config['crime']['bucket']
+    self.bucketname = "ml-crime"
     self.decompress = "unzip"
     self.parser = json.loads
   
+  def get_crime_list(self):
+    """
+    Returns all crime types.
+    """
+
+    fp = self.cache.directhandle(self.bucketname, "type_file.zip",
+           decompress="unzip")
+    return pickle.load(fp)
+
+  def get_crime_counts(self):
+    """
+    Returns the numbers of each type of crime.
+    """
+
+    fp = self.cache.directhandle(self.bucketname, "count_file.zip",
+           decompress="unzip")
+    return pickle.load(fp)
+
+  def get_region_list(self):
+    """
+    Iterates over all regions in the dataset.
+    """
+
+    fp = self.cache.directhandle(self.bucketname, "center_file.zip",
+           decompress="unzip")
+    return pickle.load(fp)
+
+  def iter(self):
+    """
+    Iterates over all crimes in the dataset.
+    """
+
+    fp = self.cache.directhandle(self.bucketname, "raw_file.zip",
+           decompress="unzip")
+
+    for date, lat, lon, crime_type in csv.reader(fp):
+      day = datetime.datetime.strptime(date, "%m/%d/%Y %I:%M:%S %p")
+      latitude = float(lat)
+      longitude = float(lon)
+      yield day, latitude, longitude, crime_type
+
+  """
   def metadata(self):
     #dfnk-7re6.json.meta.json => set0
     #ij.source.meta.meta.json => set1
@@ -36,5 +77,6 @@ class Crime(S3Iterable):
   def get_region_list(self):
     f = self.cache.directhandle(self.bucketname, 'region_list.zip', decompress="unzip")
     return pickle.load(f)
+"""
  
     
